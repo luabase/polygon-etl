ebfc09465ebb1897ad9c92b3cfc92464e68bba8f
steven-luabase <steven-luabase@Stevens-MacBook-Pro-3.local>
2022-05-27 00:22:48 -0400

    Loosen version requirements for click and python-dateutil

--- cli/setup.py
+++ cli/setup.py
@@ -37,8 +37,8 @@ setup(
         'eth-utils==1.8.4',
         'eth-abi==1.3.0',
         # TODO: This has to be removed when "ModuleNotFoundError: No module named 'eth_utils.toolz'" is fixed at eth-abi
-        'python-dateutil==2.7.0',
-        'click==7.0',
+        'python-dateutil~=2.7.0',
+        'click~=7.0',
         'ethereum-dasm==0.1.4',
         'base58',
         'requests',
