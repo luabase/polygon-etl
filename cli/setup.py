8bf530b37655a56b8c60f288246a296a2ceceb4a
steven-luabase <steven-luabase@stevens-mbp-3.lan>
2022-06-08 16:16:44 -0400

    Bumped eth-abi version requirement

--- cli/setup.py
+++ cli/setup.py
@@ -35,7 +35,7 @@ setup(
     install_requires=[
         'web3>=5.29,<6',
         'eth-utils==1.10',
-        'eth-abi==1.3.0',
+        'eth-abi==2.1.1',
         # TODO: This has to be removed when "ModuleNotFoundError: No module named 'eth_utils.toolz'" is fixed at eth-abi
         'python-dateutil~=2.7',
         'click~=7.0',
