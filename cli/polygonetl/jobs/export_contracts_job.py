57b1ff7ba0de7c3ca8f0f8f4480a94b2ff9e296f
steven-luabase <steven-luabase@stevens-mbp-3.lan>
2022-06-15 15:39:10 -0400

    Add job_id to export_contracts

--- cli/polygonetl/jobs/export_contracts_job.py
+++ cli/polygonetl/jobs/export_contracts_job.py
@@ -40,11 +40,13 @@ class ExportContractsJob(BaseJob):
             batch_size,
             batch_web3_provider,
             max_workers,
-            item_exporter):
+            item_exporter,
+            job_id=-1):
         self.batch_web3_provider = batch_web3_provider
         self.contract_addresses_iterable = contract_addresses_iterable
+        self.job_id = job_id
 
-        self.batch_work_executor = BatchWorkExecutor(batch_size, max_workers)
+        self.batch_work_executor = BatchWorkExecutor(batch_size, max_workers, job_id=self.job_id)
         self.item_exporter = item_exporter
 
         self.contract_service = EthContractService()
